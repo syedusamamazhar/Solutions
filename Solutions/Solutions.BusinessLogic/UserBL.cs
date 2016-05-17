ec2b383ef72e633ac86b194e38cc39e6ef548cde
fahad114 <fadi_114@live.com>
2016-05-17 11:46:47 -0700

    commit

---
+++ Solutions/Solutions.BusinessLogic/UserBL.cs
@@ -0,0 +1,23 @@
+﻿using Solutions.DataAccess;
+using Solutions.DataAccess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutions.BusinessLogic
+{
+    public class UserBL
+    {
+
+        public static User GetUser(string userName)
+        {
+            using (BaseRepository baseRepository = new BaseRepository())
+            {
+                //return baseRepository.Get<User>().Where(p => p.Name == userName).FirstOrDefault();
+                return baseRepository.GetIncluding<User>(p => p.SatisfactoryLevel).Where(p => p.Name.ToUpper() == userName.ToUpper()).FirstOrDefault();
+            }
+        }
+    }
+}
